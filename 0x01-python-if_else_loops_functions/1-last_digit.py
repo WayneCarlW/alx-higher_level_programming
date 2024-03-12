e86582981fb8f6d5a887ba4a83367bf6b9c07b9b
WayneCarlW <waynecarlgeno@gmail.com>
2024-03-12 22:15:30 +0200

    last nums

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -3,8 +3,8 @@ import random
 number = random.randint(-10000, 10000)
 k = number % 10
 
-print("Last digit of", end = " ")
-print("is", end = " ")
+print("Last digit of", end=" ")
+print("is", end=" ")
 print(k, end=" ")
 
 if k > 5:
@@ -13,4 +13,3 @@ elif k == 0:
     print("and is zero")
 elif k < 6 and not 0:
     print("and is less than 6 and not 0")
-    
