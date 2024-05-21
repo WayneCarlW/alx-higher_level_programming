3f23bbb0fc440c3c2b2eac4274332cc3aa486dd8
WayneCarlW <waynecarlgeno@gmail.com>
2024-05-21 22:07:38 +0300

    full description

--- 0x0D-SQL_introduction/5-full_table.sql
+++ 0x0D-SQL_introduction/5-full_table.sql
@@ -2,5 +2,4 @@
 -- Without using DESCRIBE or EXPLAIN
 SELECT COLUMN_NAME, COLUMN_TYPE, IS_NULLABLE, COLUMN_KEY,
 COLUMN_DEFAULT, EXTRA FROM information_schema.columns
-WHERE table_schema = 'hbtn_0c_0'
-and table_name = 'first_table';
+WHERE table_name = 'first_table';
